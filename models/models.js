67085ad56b9f73307ae38ecdd2b0b1f373bd6036
James Wang <j.wang5441@gmail.com>
2018-02-08 15:34:51 -0800

    Creates new project

---
+++ models/models.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
+
+// If you're getting an error here, it's probably because
+// your connect string is not defined or incorrect.
+mongoose.connect(connect);
+
+// Step 1: Write your schemas here!
+// Remember: schemas are like your blueprint, and models
+// are like your building!
+
+// Step 2: Create all of your models here, as properties.
+
+// Step 3: Export your models object
